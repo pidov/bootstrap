1c72134b323d3c96208cc7adddaf5c20cd3033c2
Ivan Pidov <ivan.pidov@gmail.com>
2018-02-06 00:39:17 +0200

    Normalize line endings

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.use((req, res, next) => {
   res.sendFile(path.resolve(TARGET_DIR, 'index.html'))
 })
 
-// app.use(express.static(TARGET_DIR))
+app.use(express.static(TARGET_DIR))
 
-app.listen(PORT, (error)  => {
+app.listen(PORT, (error) => {
   if (error) throw error
 
   console.log('Server listening on port', PORT)
