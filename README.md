f0c2d3710a6d661e3d8e2c452307187b0c2ab92b
Ivan Pidov <ivan.pidov@tick42.com>
2018-02-05 18:09:03 +0200

    Add router

---
+++ README.md
@@ -0,0 +1,1 @@
+# Loan Ticket Connect
--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+
+import createStore from './store/createStore'
 
 import '@styles'
+
+const history = createHistory()
+const store = createStore(history)
 
 export default class App extends Component {
   render () {
     return (
-      <div> Hello from App.js </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history} >
+          <div> Hello from App.js asdf</div>
+        </ConnectedRouter>
+      </Provider>
     )
   }
 }
+
