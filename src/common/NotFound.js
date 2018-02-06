bf80784f78773b453bc088fb3b8fb279a9c9a5fe
Ivan Pidov <ivan.pidov@tick42.com>
2018-02-06 13:25:08 +0200

    Notfound page styles

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -4,8 +4,8 @@ export default class NotFound extends Component {
   render () {
     return (
       <div className='page-error'>
-        <h1> 404 </h1>
-        <h2> Nothing to see here. </h2>
+        <h1>404</h1>
+        <h2>Not found</h2>
       </div>
     )
   }
